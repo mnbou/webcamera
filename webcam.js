67cb29652a61cc0f3e5de87712f955dc5f4acc19
mnbou <nacim.boussiouf@gmail.com>
2020-06-19 19:07:01 +0200

    nouveau module webcam en js

---
+++ webcam.js
@@ -0,0 +1,15 @@
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+webcam.start()
+  .then(result =>{
+    console.log("webcam started");
+  })
+  .catch(err => {
+    console.log(err);
+});
+
+let picture = webcam.snap();
+document.querySelector('#download-photo').href = picture;
